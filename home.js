fd63c19e5523561f5a33ee8141c99572a3746d82
yznaut <bigaltiffany@gmail.com>
2026-03-26 13:02:54 +0800

    Update home.js

--- home.js
+++ home.js
@@ -1,5 +1,7 @@
+// display user
 user.innerText = "User: " + localStorage.getItem("user");
 
+// switch pages
 function show(page, btn) {
     document.querySelectorAll(".main > div").forEach(d => d.style.display = "none");
     document.getElementById(page).style.display = "block";
@@ -8,40 +10,67 @@ function show(page, btn) {
     btn.classList.add("active");
 }
 
+// add currently watching
 function add() {
     let title = t.value;
     let genre = g.value;
     let date = d.value;
 
+    if (!title || !date) { alert("Title and Date required"); return; }
+
     let div = document.createElement("div");
     div.className = "card";
-
     div.innerHTML = `
-        ${title} (${genre}) - ${date}
+        <b>${title}</b> (${genre}) - ${date}
+        <br>
         <button onclick="finish(this)">Finish</button>
         <button onclick="this.parentElement.remove()">Delete</button>
     `;
-
     watching.appendChild(div);
+
+    t.value=""; g.value=""; d.value="";
 }
 
+// finish currently watching → watched
 function finish(btn) {
     let parent = btn.parentElement;
 
-    let div = document.createElement("div");
-    div.className = "card";
+    // create watched card
+    let watchedDiv = document.createElement("div");
+    watchedDiv.className = "card";
 
-    div.innerHTML = `
-        ${parent.innerText}
-        <br>Rating: <input type="number" min="1" max="5">
-        <br><input placeholder="comment">
+    let movieInfo = parent.innerText.split("Finish")[0];
+
+    // star rating HTML
+    let starsHTML = '';
+    for(let i=1;i<=5;i++){
+        starsHTML += `<span class="star" data-value="${i}">&#9734;</span>`;
+    }
+
+    watchedDiv.innerHTML = `
+        ${movieInfo}
+        <br>
+        Rate: <span class="stars">${starsHTML}</span>
+        <br>Comment: <input type="text" placeholder="Add comment">
         <button onclick="this.parentElement.remove()">Delete</button>
     `;
 
-    watched.appendChild(div);
+    watched.appendChild(watchedDiv);
     parent.remove();
+
+    // add click event to stars
+    const starEls = watchedDiv.querySelectorAll('.star');
+    starEls.forEach(star => {
+        star.addEventListener('click', function() {
+            const val = this.getAttribute('data-value');
+            starEls.forEach(s => {
+                s.innerHTML = s.getAttribute('data-value') <= val ? '&#9733;' : '&#9734;';
+            });
+        });
+    });
 }
 
+// logout
 function logout() {
-    window.location = "index.html";
+    window.location="index.html";
 }
